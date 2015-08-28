5e2f2fdf17466e9b75d22d87c71abeb7413c5834
Patryk Konior <patryk.konior@gmail.com>
2015-08-28 14:42:27 +0200

    added test for minimum expected number of articles

--- fasten/user-journeys/readTopNews.js
+++ fasten/user-journeys/readTopNews.js
@@ -3,11 +3,17 @@ var assert = require('chai').assert;
 module.exports = {
 	'Wait for front page to render': function(client) {
 		var frontPage = client.page.front();
+
 		frontPage.navigate()
+			.waitForElementVisible('body', 1000)
 			.assert.title('Aftenposten', 'title is Aftenposten')
 			.getElementSize('@topBanner', function(result) {
 				assert.isAbove(result.value.height, 239, 'minimum topBanner height');
 			});
+
+		client.elements('css selector', 'article', function(result) {
+			assert.isAbove(result.value.length, 50, 'minimum expected num of articles');
+		});
 
 		client.end();
 	}
